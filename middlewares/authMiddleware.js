cb6f0b067857d2e338b8e3be54b20880f859510b
Arseid <yvesrobert.estrada@gmail.com>
2025-02-27 13:48:41 +0100

    Modify authorization on routes for non auth users: - Login/Register OK - All GET OK

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 exports.authenticate = (req, res, next) => {
+    const publicRoutes = [
+        { method: 'POST', path: '/api/v1/auth/login' },
+        { method: 'POST', path: '/api/v1/auth/register' }
+    ];
+
+    const isPublicRoute = publicRoutes.some(route => route.method === req.method && route.path === req.path);
+
+    if (req.method === 'GET' || isPublicRoute) {
+        return next();
+    }
+
     const token = req.headers['authorization']?.split(' ')[1];
 
-    // Check if token is not provided
-    if (!token) return res.sendStatus(401).json({ message: 'Token not found' });
+    if (!token) return res.status(401).json({ message: 'Token not found' });
 
     // Verify token
     jwt.verify(token, process.env.JWT_SECRET, (err) => {
-        if (err) return res.sendStatus(401);
+        if (err) return res.status(401).json({ message: 'Invalid token' });
         next();
     });
 };
